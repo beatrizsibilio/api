ea4301832279a2123ad200eb0f9e8f3ac8ddf7d2
beatrizsibilio <beatrizduartesibilio@gmail.com>
2022-08-03 14:52:18 -0300

    criando models e rotas produtos

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'bancoProdutos',
+        'database' => 'api',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
